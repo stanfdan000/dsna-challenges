d26f05192fde452331d14947ffd78493e76beb06
sirdan <stanford303@gmail.com>
2022-09-20 10:34:34 -0700

    HOF1 CH Done

--- day-01/index.js
+++ day-01/index.js
@@ -2,4 +2,21 @@ function reverseSentence(sentence) {
   return sentence.split(' ').map(word =>
     word.split('').reverse().join('')).join(' ');
 }
-module.exports = reverseSentence;
+
+
+function addPunctuation(string) {
+  return function punctuation(q) {
+    return (`${q}${string}`);
+  };
+}
+
+
+
+
+
+
+
+
+
+
+module.exports = { reverseSentence, addPunctuation };
--- day-01/index.test.js
+++ day-01/index.test.js
@@ -1,14 +1,21 @@
-const reverseSentence = require('./index.js');
+const { reverseSentence, addPunctuation } = require('./index.js');
 
 describe('primitive code challenges', () => {
   describe('reverseSentence', () => {
-
     it('should reverse a sentence', () => {
       const output = reverseSentence('alchemy rocks gold');
       expect(output).toEqual('ymehcla skcor dlog');
     });
 
   });
+});
+
+it('add punctuation', () => {
+  const addE = addPunctuation('!!');
+  const addQ = addPunctuation('?');
+  const stop = addE('stop');
+  expect(stop).toEqual('stop!!');
+  expect(addQ('why')).toEqual('why?');
 });
 
 
